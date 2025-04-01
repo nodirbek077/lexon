b394c18f6049c7cd7212dedcc9644435ea65a594
Nodirbek <nodirbeknurqulov096@gmail.com>
2025-04-01 06:47:51 +0500

    initial commit

---
+++ src/main/java/uz/nanotechsolutions/lexon/LexonApplication.java
@@ -0,0 +1,13 @@
+package uz.nanotechsolutions.lexon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LexonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/nanotechsolutions/lexon/LexonApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.nanotechsolutions.lexon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
